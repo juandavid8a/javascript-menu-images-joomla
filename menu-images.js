8cbe2d6e21960e0ce7b21e7493895369163afdc0
Juan David ochoa <juandavid8a@gmail.com>
2019-03-07 12:55:34 -0500

    Update menu-images.js
    mejora el funcionamiento para el estado ACTIVE

--- menu-images.js
+++ menu-images.js
@@ -1,6 +1,6 @@
 // ************   Codigo para manejo de menu con imagenes (hover y active)     ************** //
 imageOver=[];	
-var items = document.getElementsByClassName("menu-over");
+var items = document.getElementsByClassName("menu-hover");
 
 var i;
 for (i = 0; i < items.length; i++) {
@@ -8,15 +8,14 @@ for (i = 0; i < items.length; i++) {
 		var img = items[i].firstChild.attributes["src"].nodeValue;
 		imageOver = img.split(".");
 		items[i].firstChild.src=imageOver[0]+'-hover.'+imageOver[1];
+		items[i].classList.remove("menu-hover");
 	}
 } 	
   
-$(".menu-over").hover(function(){
+$(".menu-hover").hover(function(){
 	var img = $(this).find('img').attr('src');
 	imageOver = img.split(".");
-	if(img != $(this).find('img').attr('src', imageOver[0]+'-hover.'+imageOver[1])){
-		$(this).find('img').attr('src', imageOver[0]+'-hover.'+imageOver[1])
-	}
+	$(this).find('img').attr('src', imageOver[0]+'-hover.'+imageOver[1])
 }, function() {
 	$(this).find('img').attr('src', imageOver[0]+'.'+imageOver[1])
 });
